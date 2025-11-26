db003d6587c6d23661c4cc92382a63c54044c3f9
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2025-11-26 14:38:10 +0530

    Add files via upload

---
+++ src/com/Java/Operators/BitwiseOperators.java
@@ -0,0 +1,20 @@
+package com.Java.Operators;
+
+public class BitwiseOperators {
+
+	public static void main(String[] args) {
+
+		int a=12,b=10;
+		System.out.println("a&b: "+(a&b)); //
+		System.out.println("a|b: "+(a|b)); //
+		System.out.println("a^b: "+(a^b)); //
+		
+		
+		
+		
+		//boolean x=true,y=false;
+		//System.out.println("x&&y: "+(x&&y)); //
+
+	}
+
+}
