b29e140ccc6609772f2d518a5b960b58f822e32c
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2025-12-04 19:58:08 +0530

    Add files via upload

---
+++ src/com/Java/LoopingStatements/Do_WhileLoop.java
@@ -0,0 +1,26 @@
+package com.Java.LoopingStatements;
+
+public class Do_WhileLoop 
+{
+	
+	public static void main(String[] args) 
+	{
+		// Print numbers from 1 to 10
+		/*
+		int i=1;
+		do
+		{
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+		*/
+		
+		int i=6;
+		do
+		{
+			System.out.println(i);
+			
+	}while(i==5); 
+  }
+
+}
