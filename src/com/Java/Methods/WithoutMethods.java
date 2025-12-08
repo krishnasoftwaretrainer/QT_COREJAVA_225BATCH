9fb0d9952996c4e607ca115b0484872a4cc0ac1b
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2025-12-08 12:18:17 +0530

    Methods Assignment and Programs

---
+++ src/com/Java/Methods/WithoutMethods.java
@@ -0,0 +1,20 @@
+package com.Java.Methods;
+
+public class WithoutMethods {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Addition is: "+c);
+		
+	
+		int a1=20;
+		int b1=10;
+		int c1=a-b;
+		System.out.println("Subtraction is: "+c1);
+		
+
+	}
+
+}
